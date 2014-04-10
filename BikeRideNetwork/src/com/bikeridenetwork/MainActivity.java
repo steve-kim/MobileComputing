24fad207fe023a8ca889d4af23769f5d48dbb2e9
Steve Kim <ElSteve05@gmail.com>
2014-04-10 15:50:23 -0500

    Can now sign in to Google+ with a Sign-In button

--- BikeRideNetwork/src/com/bikeridenetwork/MainActivity.java
+++ BikeRideNetwork/src/com/bikeridenetwork/MainActivity.java
@@ -1,22 +1,124 @@
 package com.bikeridenetwork;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Intent;
+import android.content.IntentSender.SendIntentException;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.plus.Plus;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+public class MainActivity extends Activity implements
+com.google.android.gms.common.api.GoogleApiClient.ConnectionCallbacks, com.google.android.gms.common.api.GoogleApiClient.OnConnectionFailedListener, OnClickListener {
+
+	/* Request code used to invoke sign in user interactions. */
+	private static final int RC_SIGN_IN = 0;
+
+	/* Client used to interact with Google APIs. */
+	private GoogleApiClient mGoogleApiClient;
+
+	/* A flag indicating that a PendingIntent is in progress and prevents
+	 * us from starting further intents.
+	 */
+	private boolean mIntentInProgress;
+	
+	/* Track whether the sign-in button has been clicked so that we know to resolve
+	 * all issues preventing sign-in without waiting.
+	 */
+	private boolean mSignInClicked = false;
+
+	/* Store the connection result from onConnectionFailed callbacks so that we can
+	 * resolve them when the user clicks sign-in.
+	 */
+	private ConnectionResult mConnectionResult;
+
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
 		setContentView(R.layout.activity_main);
+
+		mGoogleApiClient = new GoogleApiClient.Builder(this)
+			.addConnectionCallbacks(this)
+			.addOnConnectionFailedListener(this)
+			.addApi(Plus.API, null)
+			.addScope(Plus.SCOPE_PLUS_LOGIN)
+			.build();
+		
+		findViewById(R.id.sign_in_button).setOnClickListener(this);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	public void onConnectionFailed(ConnectionResult result) {
+		if (!mIntentInProgress) {
+		    // Store the ConnectionResult so that we can use it later when the user clicks
+		    // 'sign-in'.
+		    mConnectionResult = result;
+
+		    if (mSignInClicked) {
+		      // The user has already clicked 'sign-in' so we attempt to resolve all
+		      // errors until the user is signed in, or they cancel.
+		      resolveSignInError();
+		    }
+		  }
+		
 	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+		mSignInClicked = false;
+		Toast.makeText(this, "User is connected!", Toast.LENGTH_LONG).show();
+	}
+
+	@Override
+	public void onConnectionSuspended(int arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	/* A helper method to resolve the current ConnectionResult error. */
+	private void resolveSignInError() {
+	  if (mConnectionResult.hasResolution()) {
+	    try {
+	      mIntentInProgress = true;
+	     mConnectionResult.startResolutionForResult(this, // your activity
+                  RC_SIGN_IN);
+	    } catch (SendIntentException e) {
+	      // The intent was canceled before it was sent.  Return to the default
+	      // state and attempt to connect to get an updated ConnectionResult.
+	      mIntentInProgress = false;
+	      mGoogleApiClient.connect();
+	    }
+	  }
+	}
+	
+	protected void onActivityResult(int requestCode, int responseCode, Intent intent) {
+		  if (requestCode == RC_SIGN_IN) {
+		    if (responseCode != RESULT_OK) {
+		      mSignInClicked = false;
+		    }
+
+		    mIntentInProgress = false;
+
+		    if (!mGoogleApiClient.isConnecting()) {
+		      mGoogleApiClient.connect();
+		    }
+		  }
+		}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		if (mSignInClicked == false) {
+			mSignInClicked = true;
+			mGoogleApiClient.connect();
+		}
+		
+	}
+
 
 }
